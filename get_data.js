8a3ce1e0b93141db5516501a08c610d799b3f7a0
Mike Tahani <me@miketahani.com>
2015-02-07 20:20:39 -0800

    minor code restructuring

--- get_data.js
+++ get_data.js
@@ -13,35 +13,11 @@ var fs = require('fs'),
 
 console.log('welcome to the show');
 
-// sensor and city ids
-var ids = require('./ids.json');
-var group = function (arr, n) {
-  // divide an array into into groups containing <n> number of elements
-  var temp = [];
-  while (arr.length) {
-    temp.push(arr.splice(0, n)); 
-  }
-  return temp;
-};
-// create groups of three (floor(1000 queries per request / (60 min / 5m resolution)))
-// FIXME should calculate this every time in case you want to change the resolution
-ids.sensors = group(ids.sensors, 3).map(String);
-
-var interval = 60 * 1000,  // 60 min * 1000ms
-    queryFmt = 'YYYY-MM-DDTHH:mm:ss-0800',  // api time format as a moment-formatted string
+var queryFmt = 'YYYY-MM-DDTHH:mm:ss-0800',  // api time format as a moment-formatted string
     fileFmt  = 'YYYY_MM_DDTHH_mm_ss',   // for the filename
     debugFmt = 'HH:mm:ss',  // for console.logging status updates
-    baseUrl  = 'http://sensor-api.localdata.com/api/v1/aggregations';
-
-var get = function (url, params, filename, debugInfo, error) {
-  console.log(moment().format(debugFmt) + ': getting data ' + (debugInfo || ''));
-  request
-    .get({url: url, qs: params})
-    .on('error', error || function (err) {
-      console.log(err);
-    })
-    .pipe(fs.createWriteStream(filename));  // FIXME error checking on file-writing
-};
+    baseUrl  = 'http://sensor-api.localdata.com/api/v1/aggregations',
+    interval = 60 * 1000;  // 60 min * 1000ms
 
 // querystring parameters
 var query = {
@@ -52,35 +28,55 @@ var query = {
   before: null // later/end date
 };
 
+var group = function (arr, n) {
+  // divide an array into groups containing <n> number of elements
+  var temp = [];
+  while (arr.length) {
+    temp.push(arr.splice(0, n)); 
+  }
+  return temp;
+};
+// sensor and city ids
+var ids = require('./ids.json'),
+    // create groups of three (floor(1000 queries per request / (60 min / 5m resolution)))
+    ids = group(ids.sensors, 3).map(function (d) {
+      return {type: 'sensor', id: ''+d};
+    }).concat(ids.cities.map(function (d) {
+      return {type: 'city', id: d};
+    }));
+
+var get = function (url, params, filename, debugInfo, error) {
+  console.log('[*] ' + moment().format(debugFmt) + ': getting data ' + (debugInfo || ''));
+  request
+    .get({url: url, qs: params})
+    .on('error', error || function (err) {
+      console.log(err);
+    })
+    .pipe(fs.createWriteStream(filename));  // TODO error checking on file-writing
+};
+
 var getAllSensorData = function () {
   var toDate   = moment(),
       fromDate = toDate.clone().subtract(1, 'hour');
 
-  ['cities', 'sensors'].forEach(function (type, i) {
-    // query request type changes based on sensor request vs city request
-    var qt = (i && 'each.sources') || 'over.city';
-    ids[type].forEach(function (id) {
-      var q = JSON.parse(JSON.stringify(query));  // hella lazy
-      q.from   = fromDate.format(queryFmt);
-      q.before = toDate.format(queryFmt);
-      q[qt]    = id;
-      // i am so sorry for this
-      var filename = [
-        'data/',
-        id.replace(/\W/g, '_'),
-        '-',
-        fromDate.format(fileFmt),
-        '-',
-        toDate.format(fileFmt),
-        '.json'
-      ].join('');
-      get(baseUrl, q, filename, id); 
-    });
+  ids.forEach(function (d) {
+    var q  = JSON.parse(JSON.stringify(query));  // hella lazy
+        // query request type changes based on sensor request vs city request
+        qt = d.type === 'sensor' ? 'each.sources' : 'over.city';
+    q.from   = fromDate.format(queryFmt);
+    q.before = toDate.format(queryFmt);
+    q[qt]    = d.id;
+    // i am so sorry for this
+    var filename = [
+      'data/', d.id.replace(/\W/g, '_'), '-', 
+      fromDate.format(fileFmt), '-', toDate.format(fileFmt),
+      '.json'
+    ].join('');
+    get(baseUrl, q, filename, d.id); 
   });
 };
 
 // FIXME something better than setInterval
-setInterval(getAllSensorData, interval);
-
-
+// setInterval(getAllSensorData, interval);
+getAllSensorData();
 
